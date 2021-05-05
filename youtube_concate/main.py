5050f0ca5ec8c7b0f0e044cc1b5c765f2d44710c
Minyen Hsieh <stevehsieh2797@gmail.com>
2021-05-05 09:49:26 -0500

    Successfully read captions into data structure.

--- youtube_concate/main.py
+++ youtube_concate/main.py
@@ -1,10 +1,13 @@
+from youtube_concate.pipeline.steps.preflight import Preflight
 from youtube_concate.pipeline.steps.get_video_list import GetVideoList
 from youtube_concate.pipeline.steps.download_captions import DownloadCaptions
+from youtube_concate.pipeline.steps.read_caption import ReadCaption
+from youtube_concate.pipeline.steps.postflight import Postflight
 from youtube_concate.pipeline.steps.step import StepException
 from youtube_concate.pipeline.pipeline import Pipeline
 from youtube_concate.utils import Utils
-from youtube_concate.pipeline.steps.preflight import Preflight
-from youtube_concate.pipeline.steps.postflight import Postflight
+
+
 
 # CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 CHANNEL_ID = 'UC1W9I8YtbX4Smf6RCAfMH1Q'  # for test
@@ -19,7 +22,8 @@ def main():
         Preflight(),
         GetVideoList(),
         DownloadCaptions(),
-        Postflight()
+        ReadCaption(),
+        Postflight(),
         ]
 
     utils = Utils()
---
+++ youtube_concate/pipeline/steps/read_caption.py
@@ -0,0 +1,32 @@
+import os
+
+from pprint import pprint
+
+from .step import Step
+from youtube_concate.settings import CAPTIONS_DIR
+
+class ReadCaption(Step):
+    def process(self, data, inputs, utils):
+        data = {}
+        for caption_file in os.listdir(CAPTIONS_DIR):
+            captions = {}
+            with open(os.path.join(CAPTIONS_DIR, caption_file), 'r') as f:
+                time_line = False
+                time = None
+                for line in f:
+                    line.strip()
+                    caption = None
+                    if '-->' in line:
+                        time_line = True
+                        time = line
+                        continue
+                    if time_line:
+                        caption = line
+                        captions[caption] = time
+                        time_line = False
+            data[caption_file] = captions
+
+        pprint(data)
+        return data
+
+
